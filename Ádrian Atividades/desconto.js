f9db6ca3d369a89d4f3abc4cb11357ddd2dd69d8
adriana <152052528+adrianinhaa@users.noreply.github.com>
2026-03-31 11:42:48 -0300

    Add files via upload

---
+++ Ádrian Atividades/desconto.js
@@ -0,0 +1,14 @@
+const v = 30;
+const d = 25;
+const p = "roupa";
+
+//console.log (v - d*0.1);
+
+function desconto() {
+    const desc = (v* d *0.01);
+    console.log("O valor total é " + (v - desc)); 
+    //return desconto;
+}
+
+desconto();
+
---
+++ Ádrian Atividades/hipotenusa.js
@@ -0,0 +1,14 @@
+const c1 = 3;
+const c2 = 4;
+
+
+function resultado() {
+    soma = ((c1*c1)+(c2*c2));
+    hipo = Math.sqrt(soma);
+    return hipo;
+}
+
+const h = resultado();
+
+console.log(h);
+
